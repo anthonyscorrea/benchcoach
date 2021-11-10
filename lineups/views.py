06ceeb11a954abd6e47353e0663b856a62562b82
Tony <anthony@correa.co>
2021-11-10 17:42:43 -0600

    Added positioning to lineup view

--- lineups/views.py
+++ lineups/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.forms import formset_factory
+from .forms import PositioningForm
 from django.http import HttpResponse
 from events.models import Event
 from players.models import Player
@@ -7,5 +9,10 @@ from players.models import Player
 def edit(request, id):
     event = Event.objects.get(id=id)
     players = Player.objects.all()
+    PositioningFormSet = formset_factory(PositioningForm, extra=9)
+    formset = PositioningFormSet()
     print(event)
-    return render(request, 'lineups/lineup.html', {'title': 'Lineup', 'event': event, 'players': players, 'lineup':[]})
+    return render(request, 'lineups/lineup.html', {'title': 'Lineup',
+                                                   'event': event,
+                                                   'players': players,
+                                                   'positionings_formset':formset})
