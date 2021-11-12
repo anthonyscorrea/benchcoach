cd51a65b7aa48bb1bc26e7c959d06b4eee7e2638
Tony <anthony@correa.co>
2021-11-12 10:48:05 -0600

    reorganized fixtures (blaseball, 2021cmba). simpler now.

---
+++ load_blaseball.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PROJECT_PATH="/Users/asc/PycharmProjects_Local/benchcoach"
+PYTHON_PATH="$PROJECT_PATH/venv/bin/python"
+FIXTURES="blaseball"
+MANAGE_PY_PATH="$PROJECT_PATH/manage.py"
+bash -cl "$PYTHON_PATH $MANAGE_PY_PATH flush --no-input"
+bash -cl "$PYTHON_PATH $MANAGE_PY_PATH loaddata $FIXTURES"
