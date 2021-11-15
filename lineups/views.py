3e7b046ef4a8541f6e8bb8158e889b4970eab7bd
Tony <anthony@correa.co>
2021-11-15 08:35:07 -0600

    implement Statline, icon color for availability

--- lineups/views.py
+++ lineups/views.py
@@ -29,16 +29,17 @@ def edit(request, event_id):
         return render(request, 'success.html', {'call_back':'edit lineup','id':event_id}, status=200)
             # return render(request, 'success.html', {'call_back':'schedule'})
     event = Event.objects.get(id=event_id)
-    players = Player.objects.all().prefetch_related('availability_set')
+    players = Player.objects.all().prefetch_related('availability_set', 'statline_set')
     players = [
         {
             **model_to_dict(player),
-            'available':player.availability_set.get(event_id=event_id).get_available_display(),
-            'available_value': player.availability_set.get(event_id=event_id).available
+            'availability':player.availability_set.get(event_id=event_id),
+            # 'available_value': player.availability_set.get(event_id=event_id).available,
+            'statline': player.statline_set.get(player_id=player.id)
         }
         for player in players
     ]
-    players.sort(key=lambda d: d['available_value'], reverse = True)
+    players.sort(key=lambda d: d['availability'].available, reverse = True)
     qset = Positioning.objects.filter(event_id=event_id, order__isnull = False)
     formset = PositioningFormSet(queryset=qset)
     for form in formset:
