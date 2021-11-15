265c160f256a7f91f3015fd2ee8322a1ce74b75e
Tony <anthony@correa.co>
2021-11-15 15:57:43 -0600

    started transitioning lineup and players to tables

--- lineups/views.py
+++ lineups/views.py
@@ -29,18 +29,19 @@ def edit(request, event_id):
         return render(request, 'success.html', {'call_back':'edit lineup','id':event_id}, status=200)
             # return render(request, 'success.html', {'call_back':'schedule'})
     event = Event.objects.get(id=event_id)
-    players = Player.objects.all().prefetch_related('availability_set', 'statline_set')
+    players = Player.objects.all().prefetch_related('availability_set', 'statline_set', 'positioning_set')
     players = [
         {
             **model_to_dict(player),
             'availability':player.availability_set.get(event_id=event_id),
             # 'available_value': player.availability_set.get(event_id=event_id).available,
-            'statline': player.statline_set.get(player_id=player.id)
+            'statline': player.statline_set.get(player_id=player.id),
+            'positioning': player.positioning_set.filter(event_id=event_id).first()
         }
         for player in players
     ]
-    players.sort(key=lambda d: d['availability'].available, reverse = True)
-    qset = Positioning.objects.filter(event_id=event_id, order__isnull = False)
+    players.sort(key=lambda d: (-d['availability'].available, d['last_name']))
+    qset = Positioning.objects.filter(event_id=event_id)
     formset = PositioningFormSet(queryset=qset)
     for form in formset:
         for field in form.fields:
@@ -48,4 +49,5 @@ def edit(request, event_id):
     return render(request, 'lineups/lineup.html', {'title': 'Lineup',
                                                    'event': event,
                                                    'players': players,
+                                                   'positionings_players_initial':[player for player in players if player['positioning']],
                                                    'positionings_formset':formset})
