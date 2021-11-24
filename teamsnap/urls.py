1a17e410b2b7ae6b26a57f12f23640e1c93cd407
Tony <anthony@correa.co>
2021-11-24 16:22:12 -0600

    create a page for edit lineup based on teamsnap models

--- teamsnap/urls.py
+++ teamsnap/urls.py
@@ -7,5 +7,6 @@ from . import views
 
 urlpatterns = [
     path('events', views.EventsListView.as_view(), name="teamsnap list events"),
-    path('edit/event/<int:id>', views.edit_event, name='teamsnap edit event')
+    path('edit/event/<int:id>', views.edit_event, name='teamsnap edit event'),
+    path('edit/lineup/<int:event_id>', views.edit_lineup, name='teamsnap edit lineup')
 ]
