0b6fb7389f3fb438ee1aec6aa9b0d167432492a2
Tony <anthony@correa.co>
2021-11-24 16:20:20 -0600

    Add link to home page for teamsnap events list

--- benchcoach/views.py
+++ benchcoach/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def welcome(request):
-  pages = ['events list', 'teams list', 'venues list', 'players list']
+  pages = ['events list', 'teams list', 'venues list', 'players list', 'teamsnap list events']
   return render(request,'home.html',{'pages':pages})
