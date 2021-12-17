f436338ffc44d3c5981929b95963f21c1be7d39b
Tony <anthony@correa.co>
2021-12-17 12:48:10 -0600

    register teamsnap opponent to admin

--- teamsnap/admin.py
+++ teamsnap/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Team, Location, Event, Member, Availability
+from .models import User, Team, Location, Event, Member, Availability, Opponent
 
 # Register your models here.
 admin.site.register(User)
@@ -8,3 +8,4 @@ admin.site.register(Event)
 admin.site.register(Location)
 admin.site.register(Member)
 admin.site.register(Availability)
+admin.site.register(Opponent)
