92920b3b898efc77970c4dab28c46d41616c184c
Tony <anthony@correa.co>
2021-12-22 15:27:10 -0600

    require login

--- benchcoachproject/urls.py
+++ benchcoachproject/urls.py
@@ -18,12 +18,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from .views import welcome, user_login
+from .views import welcome, login
 
 urlpatterns = [
     path("", welcome, name="home"),
     path("", include("benchcoach.urls")),
     path("admin/", admin.site.urls),
     path("teamsnap/", include("teamsnap.urls")),
-    path("login", user_login, name="login"),
+    path("login", login, name="login"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- benchcoachproject/views.py
+++ benchcoachproject/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect, reverse, HttpResponseRedirect
 from django.contrib.auth import login,authenticate
+from django.contrib.auth.decorators import login_required
 
+@login_required()
 def welcome(request):
-  pages = ['event list', 'team list', 'venue list', 'player list', 'teamsnap list events', 'teamsnap home', 'login']
+  pages = ['event list', 'team list', 'venue list', 'player list', 'teamsnap home', 'login']
   return render(request,'home.html',{'pages':pages})
 
 
-def user_login(request):
+def login(request):
   if request.method == 'POST':
     username = request.POST.get('username')
     password = request.POST.get('password')
